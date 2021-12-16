9832cd3b4e17a20702c6f974ad0a5810bd5c16d0
mattyd2468 <matty.dryhurst@hotmail.co.uk>
2021-12-16 17:08:56 +0000

    Add plant functionality

--- src/main/java/com/matt/plantmonitor/repository/PlantLookupRepository.java
+++ src/main/java/com/matt/plantmonitor/repository/PlantLookupRepository.java
@@ -1,8 +1,13 @@
 package com.matt.plantmonitor.repository;
 
 import com.matt.plantmonitor.models.PlantLookup;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface PlantLookupRepository extends CrudRepository<PlantLookup, Integer> {
-
+    @Query(value = "SELECT ID FROM plant_lookup pl WHERE pl.name = :name",
+            nativeQuery = true)
+    int getLookupIdByName(
+            @Param("name") String name);
 }
