b20c630cc96f8c28a24863a77eed14ed7076be6d
mattyd2468 <matty.dryhurst@hotmail.co.uk>
2021-12-22 10:55:48 +0000

    Fixing sql bug where wrong reading was shown

--- src/main/java/com/matt/plantmonitor/repository/ReadingsRepository.java
+++ src/main/java/com/matt/plantmonitor/repository/ReadingsRepository.java
@@ -6,12 +6,11 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 public interface ReadingsRepository extends CrudRepository<Readings, Integer> {
-    @Query(value = "select r.ID, r.Sensor_Lookup_ID, r.Plant_Lookup_ID, r.Reading, MAX(r.timestamp) as Timestamp , s.name sensor_Name, s.unit_of_measurement \n" +
-            "from plantmonitorDB.readings r\n" +
-            "inner join plantmonitorDB.sensors s\n" +
-            "on r.sensor_lookup_id = s.id\n" +
-            "where r.plant_lookup_id = :id\n" +
-            "group by r.Sensor_Lookup_ID;",
+    @Query(value = "select r1.* , s.name sensor_Name, s.unit_of_measurement from plantmonitorDB.readings r1 \n" +
+            "             inner join plantmonitorDB.sensors s\n" +
+            "            on r1.sensor_lookup_id = s.id\n" +
+            "            where r1.timestamp in \n" +
+            "            (select MAX(r.timestamp) from plantmonitorDB.readings r where r.plant_lookup_id = :id group by r.sensor_lookup_id)",
             nativeQuery = true)
     Iterable<Readings> getLatestReadingsById(
             @Param("id") String id);
